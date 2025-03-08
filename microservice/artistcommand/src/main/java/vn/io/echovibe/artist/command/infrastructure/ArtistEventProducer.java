1c5d7e8d07e8be3b9e79eba9e52a80eb064452f5
vutiendat3601 <vutien.dat.3601@gmail.com>
2025-03-08 21:13:54 +0700

    ecv-6: configure application/json for streamBridge

--- microservice/artistcommand/src/main/java/vn/io/echovibe/artist/command/infrastructure/ArtistEventProducer.java
+++ microservice/artistcommand/src/main/java/vn/io/echovibe/artist/command/infrastructure/ArtistEventProducer.java
@@ -3,6 +3,7 @@ package vn.io.echovibe.artist.command.infrastructure;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import vn.io.echovibe.core.event.Event;
 import vn.io.echovibe.core.event.EventProducer;
@@ -16,7 +17,7 @@ public class ArtistEventProducer implements EventProducer {
   @Override
   public void produce(String topic, Event event) {
     log.info("Send event: topic=%s, eventId=%s".formatted(topic, event.getId()));
-    streamBridge.send(topic, event);
+    streamBridge.send(topic, event, MediaType.APPLICATION_JSON);
     log.info("Sent event successfully: topic=%s, eventId=%s".formatted(topic, event.getId()));
   }
 }
