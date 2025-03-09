2b07a66fb2d6d72ed18cf88dde5964d000e324d0
vutiendat3601 <vutien.dat.3601@gmail.com>
2025-03-09 12:46:57 +0700

    ecv-7: configure GitHub Actions workflow for FastAPI app

--- microservice/product/build.sh
+++ microservice/product/build.sh
@@ -4,6 +4,4 @@ BUILD_NUMBER=$(date -u +"%Y%m%d.%H%M%S")
 PYTHON_VERSION="3.11.4"
 
 docker build -t "vutiendat3601/echovibe-product:${BUILD_NUMBER}" \
--t "vutiendat3601/echovibe-product:latest" \
---platform arm64 \
-.
+-t "vutiendat3601/echovibe-product:latest" --platform arm64 .
