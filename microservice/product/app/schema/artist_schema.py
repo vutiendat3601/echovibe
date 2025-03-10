c61872afc9a6a813fd97af6740cb2223a129b431
vutiendat3601 <vutien.dat.3601@gmail.com>
2025-03-10 16:02:01 +0700

    ecv-7: fix biography alias ArtistSchema

--- microservice/product/app/schema/artist_schema.py
+++ microservice/product/app/schema/artist_schema.py
@@ -5,7 +5,7 @@ class ArtistSchema(BaseModel):
     id: str = Field(..., serialization_alias="id")
     urn: str = Field(..., serialization_alias="urn")
     name: str = Field(..., serialization_alias="name")
-    biography: str = Field(..., serialization_alias="name")
+    biography: str = Field(..., serialization_alias="biography")
     description: str = Field(..., serialization_alias="description")
     is_public: bool = Field(..., serialization_alias="isPublic")
     thumbnail_url: str = Field(..., serialization_alias="thumbnailUrl")
