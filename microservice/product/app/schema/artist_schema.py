1b2086c947165f819a9a1a9bf36a3657de7d4e7a
vutiendat3601 <vutien.dat.3601@gmail.com>
2025-03-10 16:07:51 +0700

    ecv-7: fix default value for fields in ArtistSchema

--- microservice/product/app/schema/artist_schema.py
+++ microservice/product/app/schema/artist_schema.py
@@ -2,12 +2,14 @@ from pydantic import BaseModel, Field
 
 
 class ArtistSchema(BaseModel):
-    id: str = Field(..., serialization_alias="id")
-    urn: str = Field(..., serialization_alias="urn")
-    name: str = Field(..., serialization_alias="name")
-    biography: str = Field(..., serialization_alias="biography")
-    description: str = Field(..., serialization_alias="description")
-    is_public: bool = Field(..., serialization_alias="isPublic")
-    thumbnail_url: str = Field(..., serialization_alias="thumbnailUrl")
-    background_url: str = Field(..., serialization_alias="backgroundUrl")
-    tags: list[str] = Field(..., serialization_alias="tags")
+    id: str = Field()
+    urn: str = Field()
+    name: str = Field()
+    biography: str | None = Field(default=None)
+    description: str | None = Field(default=None)
+    is_public: bool = Field(serialization_alias="isPublic")
+    thumbnail_url: str | None = Field(default=None,
+                                      serialization_alias="thumbnailUrl")
+    background_url: str | None = Field(default=None,
+                                       serialization_alias="backgroundUrl")
+    tags: list[str] = Field(default=[], serialization_alias="tags")
