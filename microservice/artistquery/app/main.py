50ba6e4a8adb2ecc67efe4199616ee96052a4dd8
vutiendat3601 <vutien.dat.3601@gmail.com>
2025-03-20 22:42:26 +0700

    ecv-17: fix wrong OpenAPI specification url

--- microservice/artistquery/app/main.py
+++ microservice/artistquery/app/main.py
@@ -39,7 +39,8 @@ class AppInitializer:
 
         self.app = FastAPI(title="Echo Vibe - Artist Query APIs",
                            version="1.0.0",
-                           lifespan=lifespan, docs_url="/v1/artists/openapi")
+                           lifespan=lifespan,
+                           openapi_url="/v1/artists/openapi")
         self.logger.info(
             f"\n{banner}\n{APP_NAME} {configuration.get_build_number()}\nPowered by FastAPI\n"
         )
