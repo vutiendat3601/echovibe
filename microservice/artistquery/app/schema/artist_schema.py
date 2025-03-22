2e9e3595365a95ad289c51b8f12384468453b614
vutiendat3601 <vutien.dat.3601@gmail.com>
2025-03-22 14:05:57 +0700

    ecv-19: update field updated_at

--- microservice/artistquery/app/schema/artist_schema.py
+++ microservice/artistquery/app/schema/artist_schema.py
@@ -3,21 +3,26 @@ from pydantic import BaseModel, Field
 
 class ArtistProfileSchema(BaseModel):
     name: str
-    description: str | None = Field(default=None,
-                                    serialization_alias="description")
-    biography: str | None = Field(default=None, serialization_alias="biography")
-    nationality_iso_code: str | None = Field(
-        default=None, serialization_alias="nationalityIsoCode")
-    thumbnail_url: str | None = Field(default=None,
-                                      serialization_alias="thumbnailUrl")
-    background_url: str | None = Field(default=None,
-                                       serialization_alias="backgroundUrl")
+    description: str | None = Field(default=None, alias="description")
+    biography: str | None = Field(default=None, alias="biography")
+    nationality_iso_code: str | None = Field(default=None,
+                                             alias="nationalityIsoCode")
+    thumbnail_url: str | None = Field(default=None, alias="thumbnailUrl")
+    background_url: str | None = Field(default=None, alias="backgroundUrl")
+
+    class Config:
+        populate_by_name = True
+        extra = "allow"
 
 
 class ArtistSchema(BaseModel):
     id: str = Field()
     urn: str = Field()
-    ref_code: str | None = Field(default=None, serialization_alias="refCode")
+    ref_code: str | None = Field(default=None, alias="refCode")
     profile: ArtistProfileSchema | None = Field(default=None)
-    is_public: bool = Field(serialization_alias="isPublic")
-    tags: list[str] = Field(default=[], serialization_alias="tags")
+    is_public: bool = Field(alias="isPublic")
+    tags: list[str] = Field(default=[], alias="tags")
+
+    class Config:
+        populate_by_name = True
+        extra = "allow"
