924be7ab05da66cd935cfedbaac70c49cb17603e
vutiendat3601 <vutien.dat.3601@gmail.com>
2025-03-22 12:31:26 +0700

    ecv-19: map field image URLs, nationality_iso_code

--- microservice/artistquery/app/mapper/artist_mapper.py
+++ microservice/artistquery/app/mapper/artist_mapper.py
@@ -1,13 +1,21 @@
-from app.model.artist import Artist, ArtistProfile
+from app.model.artist import Artist
 from app.schema.artist_schema import ArtistSchema, ArtistProfileSchema
 
 
 def map_to_artist_schema(artist: Artist):
-    profile: ArtistProfileSchema = ArtistProfileSchema(
-        **artist.profile.model_dump())
+    # TODO: map thumbnail_file_key to thumbnail_url in case thumbnail_url is null.
+    # TODO: map background_file_key to background_url in case background_url is null.
+    profile = artist.profile
+    profile_schema: ArtistProfileSchema = ArtistProfileSchema(
+        name=profile.name,
+        biography=profile.biography,
+        description=profile.description,
+        nationality_iso_code=profile.nationality_iso_code,
+        thumbnail_url=profile.thumbnail_url,
+        background_url=profile.background_url)
     return ArtistSchema(id=artist.aggregate_id,
                         urn=artist.urn,
                         ref_code=artist.ref_code,
-                        profile=profile,
                         is_public=artist.is_public,
-                        tags=artist.tags)
+                        tags=artist.tags,
+                        profile=profile_schema)
