bfce37350d4f8b8ae350fed66ca18ecb499ce15f
vutiendat3601 <vutien.dat.3601@gmail.com>
2025-04-07 15:32:16 +0700

    ecv-16: set order by updated_at desc at Get all artists API in artistquery project

--- microservice/artistquery/app/repository/artist_repository.py
+++ microservice/artistquery/app/repository/artist_repository.py
@@ -5,6 +5,13 @@ from app.model.artist import Artist
 class ArtistRepository(ABC):
 
     @abstractmethod
+    def find_all_by_is_active_true(
+            self,
+            is_load_images: bool = False,
+            is_load_revisions: bool = True) -> list[Artist]:
+        """Find all active Artists"""
+
+    @abstractmethod
     def find_by_aggregate_id_and_is_active_true(
             self,
             aggregate_id: str,
@@ -35,10 +42,3 @@ class ArtistRepository(ABC):
     @abstractmethod
     def delete_by_aggregate_id(self, aggregate_id: str) -> None:
         """Delete Artist by aggregate_id"""
-
-    @abstractmethod
-    def find_all_by_is_active_true(
-            self,
-            is_load_images: bool = False,
-            is_load_revisions: bool = True) -> list[Artist]:
-        """Find all active Artists"""
