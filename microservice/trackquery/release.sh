37ce3eae400ae2c64a758d91cfa3d2c5f6f1134f
vutiendat3601 <vutien.dat.3601@gmail.com>
2025-04-12 14:50:15 +0700

    ecv-11: configure default container port for trackquery project

--- microservice/trackquery/release.sh
+++ microservice/trackquery/release.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 BUILD_NUMBER=$(date -u +"%Y%m%d.%H%M%S")
-PYTHON_VERSION="3.11.4"
 
 docker build -t "vutiendat3601/echovibe-trackquery:${BUILD_NUMBER}" -t "vutiendat3601/echovibe-trackquery:latest" \
 --build-arg BUILD_NUMBER=${BUILD_NUMBER} . &&
