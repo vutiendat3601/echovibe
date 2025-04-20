b482d3d1152f73df9bc973b9aa80fd419dd29d48
vutiendat3601 <vutien.dat.3601@gmail.com>
2025-04-20 18:24:45 +0700

    ecv-28: fix ArtistProfileSchema not found

--- microservice/product/app/event/schema/artist_event_schema.py
+++ microservice/product/app/event/schema/artist_event_schema.py
@@ -1,7 +1,24 @@
 from pydantic import BaseModel, Field
 from app.event.schema.event_schema import EventSchema
 from app.schema.tag_schema import TagSchema
-from app.schema.artist_schema import ArtistProfileSchema
+
+
+class ArtistProfileSchema(BaseModel):
+    name: str
+    description: str | None = Field(default=None, alias="description")
+    biography: str | None = Field(default=None, alias="biography")
+    nationality_iso_code: str | None = Field(default=None,
+                                             alias="nationalityIsoCode")
+    thumbnail_file_key: str | None = Field(default=None,
+                                           alias="thumbnailFileKey")
+    thumbnail_url: str | None = Field(default=None, alias="thumbnailUrl")
+    background_file_key: str | None = Field(default=None,
+                                            alias="backgroundFileKey")
+    background_url: str | None = Field(default=None, alias="backgroundUrl")
+
+    class Config:
+        populate_by_name = True
+        extra = "allow"
 
 
 class ArtistCreatedEvent(EventSchema):
