c2351cec5d17e66f44df40e54853db751b6ac02e
vutiendat3601 <vutien.dat.3601@gmail.com>
2025-05-13 17:21:49 +0700

    ecv-36: configure get_track_stats_by_ids API

--- microservice/activity/app/router/track_router.py
+++ microservice/activity/app/router/track_router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
 from app.core.container import Container
 from dependency_injector.wiring import Provide
 from app.core.logger import Logger
@@ -7,6 +7,7 @@ from app.schema.schema import ResponseSchema, ok
 from app.schema.track_schema import TrackStatsSchema
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
+from app.constant.constant import AGGREGATE_ID_LIST_REGEX
 
 track_router = APIRouter(prefix="/v1/tracks", tags=["Track"])
 
@@ -18,5 +19,15 @@ track_service: TrackService = Provide[Container.track_service]
                   response_model=ResponseSchema[TrackStatsSchema])
 async def get_track_stats(id: str):
     track_stats_schema = await track_service.get_track_stats(id)
-    response_scheme = ok(data=track_stats_schema)
-    return JSONResponse(content=jsonable_encoder(response_scheme))
+    response_schema = ok(data=track_stats_schema)
+    return JSONResponse(content=jsonable_encoder(response_schema))
+
+
+@track_router.get(path="/stats",
+                  response_model=ResponseSchema[TrackStatsSchema])
+async def get_track_stats_by_ids(ids: str = Query(
+    ..., regex=AGGREGATE_ID_LIST_REGEX)):
+    track_stats_schemas = await track_service.get_track_stats_by_ids(
+        ids.split(","))
+    response_schema = ok(data=track_stats_schemas)
+    return JSONResponse(content=jsonable_encoder(response_schema))
